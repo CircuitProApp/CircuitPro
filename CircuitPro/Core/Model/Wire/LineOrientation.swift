ca49cfab6702f30c566c1750377893497d705f55
Giorgi T <bulbs.carrots_0a@icloud.com>
2025-08-14 00:20:54 +0400

    REF: Removed xcscheme related stuff

---
+++ CircuitPro/Core/Model/Wire/LineOrientation.swift
@@ -0,0 +1,13 @@
+//
+//  LineOrientation.swift
+//  CircuitPro
+//
+//  Created by Giorgi Tchelidze on 7/9/25.
+//
+
+import Foundation
+
+enum LineOrientation {
+    case horizontal
+    case vertical
+}
