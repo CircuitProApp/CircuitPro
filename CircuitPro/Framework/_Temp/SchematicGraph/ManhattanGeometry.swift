a64885af42c4a349176a6dd6ac1163e6b155fd55
Giorgi T <bulbs.carrots_0a@icloud.com>
2025-08-27 18:11:13 +0400

    REF: GraphKit Phase 2 - restructure directory, cleanup. Done with this refactor for now

--- CircuitPro/Framework/_Temp/SchematicGraph/ManhattanGeometry.swift
+++ CircuitPro/Framework/_Temp/SchematicGraph/ManhattanGeometry.swift
@@ -1,5 +1,5 @@
 //
-//  ManhattanGrid.swift
+//  ManhattanGeometry.swift
 //  CircuitPro
 //
 //  Created by Giorgi Tchelidze on 8/26/25.
@@ -7,17 +7,17 @@
 
 import CoreGraphics
 
-public struct ManhattanGeometry: GeometryPolicy {
-    public let step: CGFloat
-    public let epsilon: CGFloat
-    public var neighborhoodPadding: CGFloat { step }
+struct ManhattanGeometry: GeometryPolicy {
+    let step: CGFloat
+    let epsilon: CGFloat
+    var neighborhoodPadding: CGFloat { step }
 
-    public init(step: CGFloat, epsilon: CGFloat? = nil) {
+    init(step: CGFloat, epsilon: CGFloat? = nil) {
         self.step = step
         self.epsilon = epsilon ?? step * 0.01
     }
 
-    public func snap(_ p: CGPoint) -> CGPoint {
+    func snap(_ p: CGPoint) -> CGPoint {
         CGPoint(
             x: (p.x / step).rounded() * step,
             y: (p.y / step).rounded() * step
@@ -25,12 +25,12 @@ public struct ManhattanGeometry: GeometryPolicy {
     }
 
     // Axis-aligned unit directions
-    public func admissibleDirections() -> [CGVector] {
+    func admissibleDirections() -> [CGVector] {
         [CGVector(dx: 1, dy: 0), CGVector(dx: 0, dy: 1)]
     }
 
     // Collinearity test with tolerance
-    public func isCollinear(a: CGPoint, b: CGPoint, dir: CGVector) -> Bool {
+    func isCollinear(a: CGPoint, b: CGPoint, dir: CGVector) -> Bool {
         let vx = b.x - a.x, vy = b.y - a.y
         let cross = dir.dx * vy - dir.dy * vx
         let scale = max(hypot(dir.dx, dir.dy), epsilon)
@@ -38,7 +38,7 @@ public struct ManhattanGeometry: GeometryPolicy {
     }
 
     // Parametric projection along a direction
-    public func projectParam(origin: CGPoint, dir: CGVector, point: CGPoint) -> CGFloat {
+    func projectParam(origin: CGPoint, dir: CGVector, point: CGPoint) -> CGFloat {
         let dx = point.x - origin.x, dy = point.y - origin.y
         let denom = max(dir.dx * dir.dx + dir.dy * dir.dy, epsilon * epsilon)
         return (dx * dir.dx + dy * dir.dy) / denom
